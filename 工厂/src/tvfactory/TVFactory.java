54360bae5da7f8f4591bef4ce66dec186d59de73
184805073 <55056109+184805073@users.noreply.github.com>
2019-10-09 22:35:36 +0800

    工厂建包

---
+++ 工厂/src/tvfactory/TVFactory.java
@@ -0,0 +1,13 @@
+package tvfactory;
+
+import tv.*;
+
+public interface TVFactory {
+	public static Tv produceTv(String brand){
+		if(brand.equals("HaierTv"))
+			return new haierTv();
+		if(brand.equals("HisenseTv"))
+			return new HisenseTv();
+		return null;
+	}
+}
