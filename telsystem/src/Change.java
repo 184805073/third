4df27612a7b632b55d2fd95cc8df5d2615df9442
184805073 <55056109+184805073@users.noreply.github.com>
2019-11-18 23:22:08 +0800

    第11周

---
+++ telsystem/src/Change.java
@@ -0,0 +1,26 @@
+import java.util.Date;
+import java.util.Random;
+import factory.*;
+import species.*;
+import utility.XMLUtility;
+
+public class Change {
+
+	public static void main(String[] args) {
+		
+			YiDongCompany c1=new YiDongCompany();
+			LianTongCompany c2=new LianTongCompany();
+			
+			LianTong s2=c2.createSorts();
+			YiDong s1=c1.createSorts();
+       
+			s1.generateCommunicateRecord();
+			s2.generateCommunicateRecord();
+		  
+			s1.printDetails();
+			s2.printDetails();
+		  
+	}
+	
+
+}
